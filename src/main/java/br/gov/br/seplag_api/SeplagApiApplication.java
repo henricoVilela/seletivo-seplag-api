d9e403ca597205d0d8d307b1548d23b19e22ec2b
Henrico Vilela <henricoVilela@gmail.com>
2025-03-19 19:43:40 -0400

    Inicializando projeto spring

---
+++ src/main/java/br/gov/br/seplag_api/SeplagApiApplication.java
@@ -0,0 +1,13 @@
+package br.gov.br.seplag_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeplagApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeplagApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/br/seplag_api/SeplagApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.br.seplag_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeplagApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
