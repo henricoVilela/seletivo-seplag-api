1216e011cc53778ddf49e0d241116ed6d0a878db
Henrico Vilela <henricoVilela@gmail.com>
2025-03-20 22:12:36 -0400

    Listagem paginada de servidores efetivos

---
+++ src/main/java/br/gov/br/seplag_api/api/controller/ServidorEfetivoController.java
@@ -0,0 +1,32 @@
+package br.gov.br.seplag_api.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.gov.br.seplag_api.api.model.ServidorEfetivoDTO;
+import br.gov.br.seplag_api.domain.service.ServidorEfetivoService;
+
+@RestController
+@RequestMapping("/api/servidore-efetivo")
+public class ServidorEfetivoController {
+	
+	@Autowired
+    private ServidorEfetivoService servidorEfetivoService;
+	
+	@GetMapping
+    public ResponseEntity<Page<ServidorEfetivoDTO>> listarTodos(
+            @RequestParam(defaultValue = "0") int pagina,
+            @RequestParam(defaultValue = "10") int tamanho,
+            @RequestParam(defaultValue = "id") String ordenacao,
+            @RequestParam(defaultValue = "ASC") String direcao) {
+        
+        Page<ServidorEfetivoDTO> servidores = servidorEfetivoService.listarTodosPaginado(pagina, tamanho, ordenacao, direcao);
+        return ResponseEntity.ok(servidores);
+    }
+
+}
