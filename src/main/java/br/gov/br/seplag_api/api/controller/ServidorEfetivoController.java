1a24086f89a328bb261d53484a48f09dab9b7d83
Henrico Vilela <henricoVilela@gmail.com>
2025-03-22 19:08:07 -0400

    Adiciona endpoint de busca por id

--- src/main/java/br/gov/br/seplag_api/api/controller/ServidorEfetivoController.java
+++ src/main/java/br/gov/br/seplag_api/api/controller/ServidorEfetivoController.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,6 +32,13 @@ public class ServidorEfetivoController {
         
         Page<ServidorEfetivoDTO> servidores = servidorEfetivoService.listarTodosPaginado(pagina, tamanho, ordenacao, direcao);
         return ResponseEntity.ok(servidores);
+    }
+	
+	@GetMapping("/{id}")
+    public ResponseEntity<ServidorEfetivoDTO> buscarPorId(@PathVariable Integer id) {
+        return servidorEfetivoService.buscarPorId(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 	
 	@PostMapping
