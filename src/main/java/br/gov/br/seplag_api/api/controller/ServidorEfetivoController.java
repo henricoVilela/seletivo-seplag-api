3b6ecdda7c7f7bf0920a4770c44dd5e4920b4531
Henrico Vilela <henricoVilela@gmail.com>
2025-03-22 19:39:34 -0400

    Adiciona endpoint de exclusão de servidor

--- src/main/java/br/gov/br/seplag_api/api/controller/ServidorEfetivoController.java
+++ src/main/java/br/gov/br/seplag_api/api/controller/ServidorEfetivoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -54,6 +55,14 @@ public class ServidorEfetivoController {
         return servidorEfetivoService.atualizar(servidorDTO)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
+    }
+	
+	@DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluir(@PathVariable Integer id) {
+        if (servidorEfetivoService.excluir(id)) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
     }
 
 }
