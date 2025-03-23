e1148be7addae45347e95c8c74ec4f025e776241
Henrico Vilela <henricoVilela@gmail.com>
2025-03-22 23:06:54 -0400

    Refatorando estrutura das classe de servidores

--- src/main/java/br/gov/br/seplag_api/api/model/ServidorTemporarioDTO.java
+++ src/main/java/br/gov/br/seplag_api/api/model/ServidorTemporarioDTO.java
@@ -1,7 +1,6 @@
 package br.gov.br.seplag_api.api.model;
 
 import java.time.LocalDate;
-import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -10,37 +9,12 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 import br.gov.br.seplag_api.api.model.converter.LocalDateDeserializer;
 import br.gov.br.seplag_api.api.model.converter.LocalDateSerializer;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
-public class ServidorTemporarioDTO {
+public class ServidorTemporarioDTO extends PessoaDto {
 	public Integer id;
 	
-	@NotBlank(message = "não pode estar em branco")
-    @Size(min = 3, max = 200, message = "O nome deve ter entre {min} e {max} caracteres")
-	public String nome;
-	
-	@JsonSerialize(using = LocalDateSerializer.class)
-	@JsonDeserialize(using = LocalDateDeserializer.class)
-	@NotNull(message = "não pode estar em branco")
-    public LocalDate dataNascimento;
-    
-    @NotBlank(message = "não pode estar em branco")
-    @Size(min = 1, max = 1, message = "O nome deve ter entre {min} e {max} caracteres")
-    public String sexo;
-    
-    @NotBlank(message = "não pode estar em branco")
-    @Size(min = 3, max = 200, message = "O nome deve ter entre {min} e {max} caracteres")
-    public String mae;
-    
-    @NotBlank(message = "não pode estar em branco")
-    @Size(min = 3, max = 200, message = "O nome deve ter entre {min} e {max} caracteres")
-    public String pai;
-    
     @JsonSerialize(using = LocalDateSerializer.class)
 	@JsonDeserialize(using = LocalDateDeserializer.class)
     public LocalDate dataAdmissao;
@@ -49,6 +23,4 @@ public class ServidorTemporarioDTO {
 	@JsonDeserialize(using = LocalDateDeserializer.class)
     public LocalDate dataDemissao;
     
-    @NotEmpty(message = "não pode estar vazio")
-    public List<EnderecoDto> enderecos;
 }
